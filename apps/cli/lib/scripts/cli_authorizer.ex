5f40f8d2365db59676637d93bd32635c50fdaa1e
gabriel <gabri005@gmail.com>
2021-09-22 16:48:44 -0300

    added task stream

--- apps/cli/lib/scripts/cli_authorizer.ex
+++ apps/cli/lib/scripts/cli_authorizer.ex
@@ -14,70 +14,67 @@ defmodule Cli.Scripts.Authorizer do
   def main(_) do
     Logger.info("getting file info and processing")
 
-    data =
+    rendered_data =
       Stdin.read_data()
       |> Enum.map(&Jason.decode!(&1))
       |> execute()
       |> AccountRender.render()
 
+    handle_value(rendered_data)
+
+    rendered_data
+  end
+
+  defp handle_value(data) do
     if Mix.env() != :test do
       Enum.each(data, &IO.puts/1)
     end
-
-    data
+  rescue
+    UndefinedFunctionError -> Enum.each(data, &IO.puts/1)
   end
 
   defp execute(data) do
     data
     |> AuhtorizerReader.re()
     |> Enum.map(&process_results/1)
-
-    # |> Task.async_stream(& &1. (),
-    #   max_concurrency: 40,
-    #   timeout: 10_000
-    # )
-    # |> get_tasks_results()
+    |> Task.async_stream(& &1. (),
+      max_concurrency: 40,
+      timeout: 10_000
+    )
+    |> get_tasks_results()
   end
 
-  # defp get_tasks_results(data) do
+  defp get_tasks_results(data) do
+    Enum.reduce_while(data, {:ok, []}, fn
+      {:ok, {:error, _} = error}, _ ->
+        {:halt, error}
 
-  #   Enum.reduce(data, [], fn result, acc ->
-  #     # {:ok, {:error, _} = error}, _ ->
-  #     #   {:halt, error}
+      {:ok, [_, {:error, _} = error]}, _ ->
+        {:halt, error}
 
-  #     # {:ok, [_, {:error, _} = error]}, _ ->
-  #     #   {:halt, error}
+      {:ok, result}, {:ok, acc} ->
+        {:cont, {:ok, [result | acc]}}
+    end)
+    |> case do
+      {:error, _} = error ->
+        error
 
-  #     # {:ok, result}, {:ok, acc} ->
-  #     #   {:cont, [result | acc]}
-  #     # result when is_list(result) ->
-  #     #   [result | acc]
-  #   end)
-  # end
+      {:ok, results} ->
+        results
+        |> List.flatten()
+    end
+  end
 
   defp process_results({account, transactions, operation})
        when operation in [
               "accounts_with_transactions",
               "non_initialized_accounts_with_transactions"
             ] do
-    AuthorizeTransactions.execute(account, transactions)
+    fn -> AuthorizeTransactions.execute(account, transactions) end
   end
 
   defp process_results({account, accounts, "accounts"}) do
-    [account | accounts]
-    |> CreateAccount.execute()
+   fn -> [account | accounts]
+    |> CreateAccount.execute() end
   end
-
-  # defp process_results({account, transactions, operation})
-  #      when operation in [
-  #             "accounts_with_transactions",
-  #             "non_initialized_accounts_with_transactions"
-  #           ] do
-  #   fn -> AuthorizeTransactions.execute(account, transactions) end
-  # end
-
-  # defp process_results({account, accounts, "accounts"}) do
-  #  fn -> [account | accounts]
-  #   |> CreateAccount.execute() end
-  # end
 end
